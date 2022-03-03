8149ae2519c680112ab896413cd22a1eafa2de8b
owuraku-zenas <awukuzenas@gmail.com>
2022-03-03 17:07:59 +0000

    colapseable sections (complete and incomplete)

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -1,7 +1,12 @@
 import "../styles/TodoItems.css";
 import TodoItem from "./TodoItem";
+import { HiChevronDown, HiChevronRight } from "react-icons/hi";
+import { useState } from "react";
 
 function TodoItems({ todos, onDelete }) {
+
+    const [doneSectionToggle, setDoneSectionToggle] = useState(true)
+    const [UnDoneSectionToggle, setUnDoneSectionToggle] = useState(true)
 
     if (todos.length < 1) {
         return (
@@ -16,28 +21,38 @@ function TodoItems({ todos, onDelete }) {
     return (
         <div className="todo__items__area">
             <div className="section">
-                <span className="section__name">
+                <span className="section__name" onClick={ () => setUnDoneSectionToggle(!UnDoneSectionToggle) } >
+                    { UnDoneSectionToggle ? <HiChevronDown /> : <HiChevronRight /> }
                     Undone
                 </span>
+                {
+                    UnDoneSectionToggle ?
+
                 <div className="todo__items">
                     {
                         todos.filter((todo) => todo.completed === false).map((todo) => (
                             <TodoItem key={todo.id} todo={todo} onDelete={onDelete} />
                         ))
                     }
-                </div>
+                </div> : null
+                }
             </div>
             <div className="section">
-                <span className="done">
+                <span className="section__name"  onClick={ () => setDoneSectionToggle(!doneSectionToggle) }>
+                    { doneSectionToggle ? <HiChevronDown /> : <HiChevronRight /> }
                     Done
                 </span>
+                {
+                    doneSectionToggle ?
+
                 <div className="todo__items">
                     {
                         todos.filter((todo) => todo.completed === true).map((todo) => (
                             <TodoItem key={todo.id} todo={todo} onDelete={onDelete} />
                         ))
                     }
-                </div>
+                </div> : null
+                }
             </div>
         </div>
 
